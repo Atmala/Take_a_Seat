c8b3416c269f9829228377451d732e0ac69b4803
Kate Astafieva <kastafieva@dio-soft.com>
2014-07-07 14:06:24 +0300

    struggling with paper

--- TakeSeat/TakeSeat/js/paperPOC.js
+++ TakeSeat/TakeSeat/js/paperPOC.js
@@ -1,10 +1,14 @@
 ﻿
 
-$(document).ready(function() {
-   // var path = new Path();
+/*$(document).ready(function() {
+    var path = new paper.Path({
+        strokeColor: '#E4141B',
+        strokeWidth: 20,
+        strokeCap: 'round'
+    });
 
     $('.paperCanvas').mousedown(function(e) {
-        alert("start");
+        //alert("start");
         // Give the stroke a color
         path.strokeColor = 'black';
         var start = new Point(e.offsetX, e.offsetY);
@@ -14,4 +18,5 @@ $(document).ready(function() {
     $('.paperCanvas').mousedown(function(e) {
         path.add(new Point(e.offsetX, e.offsetY));
     });
-});
+});*/
+
