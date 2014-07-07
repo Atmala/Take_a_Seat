f1e89432487e73ba449460051460437217091d34
Kate Astafieva <kastafieva@dio-soft.com>
2014-07-07 08:58:51 +0300

    POC for Paper and stub for Easel

---
+++ TakeSeat/TakeSeat/js/paperPOC.js
@@ -0,0 +1,17 @@
+﻿
+
+$(document).ready(function() {
+   // var path = new Path();
+
+    $('.paperCanvas').mousedown(function(e) {
+        alert("start");
+        // Give the stroke a color
+        path.strokeColor = 'black';
+        var start = new Point(e.offsetX, e.offsetY);
+        path.moveTo(start);
+    });
+
+    $('.paperCanvas').mousedown(function(e) {
+        path.add(new Point(e.offsetX, e.offsetY));
+    });
+});
