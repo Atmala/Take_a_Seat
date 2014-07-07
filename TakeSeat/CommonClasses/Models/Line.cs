901cc7feaf54dd2e004ff091107b75603b6a6baa
urise <urise@i.ua>
2014-07-07 18:15:00 +0300

    RoomModel is added

---
+++ TakeSeat/CommonClasses/Models/Line.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClasses.Models
+{
+    public class Line
+    {
+        public int X1 { get; private set; }
+        public int Y1 { get; private set; }
+        public int X2 { get; private set; }
+        public int Y2 { get; private set; }
+
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+        }
+    }
+}
---
+++ TakeSeat/CommonClasses/Models/RoomModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonClasses.Models
+{
+    public class RoomModel
+    {
+        private readonly List<Line> _lines = new List<Line>(); 
+        public IEnumerable<Line> Lines { get { return _lines.AsEnumerable(); } }
+
+        public void AddLine(Line line)
+        {
+            _lines.Add(line);
+        }
+
+        public void Clear()
+        {
+            _lines.Clear();
+        }
+
+        public void CreateTestData()
+        {
+            Clear();
+            AddLine(new Line(-100, -100, 100, -100));
+            AddLine(new Line(100, -100, 100, 100));
+            AddLine(new Line(100, 100, -100, 100));
+            AddLine(new Line(-100, 100, -100, -100));
+            AddLine(new Line(-50, -100, -50, 0));
+            AddLine(new Line(-50, 0, 50, 0));
+            AddLine(new Line(50, 0, 50, -100));
+        }
+    }
+}
