5b17e9a9b950f73502272ead35257d6923b0cdeb
Kate Astafieva <kastafieva@dio-soft.com>
2014-07-25 11:55:47 +0300

    Line positioning fixed

--- TakeSeat/TakeSeat/js/Controllers/MapController.js
+++ TakeSeat/TakeSeat/js/Controllers/MapController.js
@@ -10,28 +10,31 @@ function PaperController($scope){
         paper.setup(canvas[0]);
     }
 
+    $scope.mouseDown = function (event) {
+        $scope.isDrawing = true;
+        $scope.path = new paper.Path();
+
+        $scope.path.strokeColor = '#ACCCE2';
+        $scope.start = new paper.Point([event.offsetX, event.offsetY]);
+
+    };
+
     $scope.mouseMove = function(event) {
         if ($scope.isDrawing) {
+            var x = event.offsetX;
+            var y = event.offsetY;
+            if (x <= 5 || y <= 5 || x >= event.currentTarget.width-5 || y >= event.currentTarget.height-5) {
+                $scope.mouseUp();
+                return;
+            }
+
             $scope.path.removeSegments();
 
             $scope.path.moveTo($scope.start);
-            var x = event.clientX - $scope.offset.left;
-            var y = event.clientY - $scope.offset.top;
-            $scope.path.lineTo($scope.start.add([x,y]));
+            $scope.path.lineTo(new paper.Point([x, y]));
 
             paper.view.draw();
         }
-    };
-
-    $scope.mouseDown = function(event) {
-        $scope.isDrawing = true;
-        $scope.path = new paper.Path();
-       
-        $scope.path.strokeColor = 'black';
-        var x = event.clientX - $scope.offset.left;
-        var y = event.clientY - $scope.offset.top;
-        $scope.start = new paper.Point([x, y]);
-     
     };
 
     $scope.mouseUp = function (event) {
@@ -39,43 +42,3 @@ function PaperController($scope){
     };
 
 };
-
-//app.directive('draw', function () {
-//    return {
-//        restrict: 'AEC',
-//        link: function postLink(scope, element, attrs) {
-//            var path;
-//            var drag = false;
-
-//            function mouseUp(event) {
-//                //Clear Mouse Drag Flag
-//                drag = false;
-//            }
-
-//            function mouseDrag(event) {
-//                if (drag) {
-//                    path.add(new paper.Point(event.layerX, event.layerY));
-//                    path.smooth();
-//                }
-//            }
-
-//            function mouseDown(event) {
-//                //Set  flag to detect mouse drag
-//                drag = true;
-//                path = new paper.Path();
-//                path.strokeColor = 'white';
-//                path.add(new paper.Point(event.layerX, event.layerY));
-//            }
-
-//            function initPaper() {
-//                paper.install(window);
-//                paper.setup('canvas');
-//            }
-
-//            element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
-
-//            initPaper();
-
-//        }
-//    };
-//});
