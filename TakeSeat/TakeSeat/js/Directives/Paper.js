34f3a8d80d41cf39192e4a7bfab1d07eebfe60de
Kate Astafieva <kastafieva@dio-soft.com>
2014-07-30 09:50:39 +0300

    Drawing lines from server

--- TakeSeat/TakeSeat/js/Directives/Paper.js
+++ TakeSeat/TakeSeat/js/Directives/Paper.js
@@ -27,30 +27,40 @@ seatApp
 
                             path.removeSegments();
 
-                            drawLine();
+                            drawLine(start, new paper.Point([x, y]));
                         }
                     }
 
-                    function drawLine() {
-                        path.moveTo(start);
-                        path.lineTo(new paper.Point([x, y]));
+                    function drawLine(startPoint, endPoint) {
+                        path.moveTo(startPoint);
+                        path.lineTo(endPoint);
 
                         paper.view.draw();
                     }
 
+                    function getNewPath() {
+                        var newPath = new paper.Path();
+                        newPath.strokeColor = '#ACCCE2';
+                        return newPath;
+                    }
+
                     function mouseDown(event) {
                         isDrawing = true;
-                        path = new paper.Path();
-
-                        path.strokeColor = '#ACCCE2';
+                        path = getNewPath();
                         start = new paper.Point([event.offsetX, event.offsetY]);
-
                     }
 
                     function initPaper() {
                         paper.install(window);
                         var canvas = $('#paperCanvas');
                         paper.setup(canvas[0]);
+
+                        scope.$watch('scope.lines', function() {
+                            _.each(scope.lines, function(line) {
+                                path = getNewPath();
+                                drawLine(new paper.Point([line.X1, line.Y1]), new paper.Point([line.X2, line.Y2]));
+                            });
+                        });
                     }
 
                     element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
