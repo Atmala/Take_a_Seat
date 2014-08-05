10bc31f6d47c059e1eb48df5d22cfb92f3e88fd4
urise <urise@i.ua>
2014-08-05 10:33:57 +0300

    DB Test

--- TakeSeat/TakeSeat.Tests/DbLayerTemporaryTests/BaseDbTests.cs
+++ TakeSeat/TakeSeat.Tests/DbLayerTemporaryTests/BaseDbTests.cs
@@ -18,8 +18,23 @@ namespace TakeSeat.Tests.DbLayerTemporaryTests
         {
             using (var db = new TakeSeatDbContext())
             {
-                db.Rooms.Add(new Room {Caption = "Test Room", Order = 1});
+                var room = new Room {Caption = "Test Room", Order = 1};
+                db.Rooms.Add(room);
                 db.SaveChanges();
+
+                var roomObject = new RoomObject { RoomObjectTypeId = 2, RoomId = room.Id };
+                db.RoomObjects.Add(roomObject);
+                db.SaveChanges();
+            }
+        }
+        [Test]
+        public void ReadRoom()
+        {
+            using (var db = new TakeSeatDbContext())
+            {
+                var roomId = db.Rooms.Max(r => r.Id);
+                db.Configuration.LazyLoadingEnabled = true;
+                var room = db.Rooms.FirstOrDefault(r => r.Id == roomId);
             }
         }
     }
