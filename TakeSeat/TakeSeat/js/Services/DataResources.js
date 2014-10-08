35c0fd6c24dec262044278b0a74baccd1a0840ee
Yuriy Dolzhikov <urise@i.ua>
2014-10-09 01:22:56 +0300

    IIS Integration

--- TakeSeat/TakeSeat/js/Services/DataResources.js
+++ TakeSeat/TakeSeat/js/Services/DataResources.js
@@ -1,22 +1,25 @@
 ﻿angular.module('DataResources', ['ngResource']).
     factory('MapProvider', [
         '$resource', function($resource) {
-            return $resource('/Map/:action', {}, {
-                'Get': { method: 'GET', params: { action: 'Get' } },
-                'SaveRoom': { method: 'POST', params: { action: 'SaveRoom' } },
-                'SaveWall': { method: 'POST', params: { action: 'SaveWall' } },
-                'SaveTable': { method: 'POST', params: { action: 'SaveTable' } },
-                'SaveEmployeeTableLink': { method: 'POST', params: { action: 'SaveEmployeeTableLink' } },
-                'RemoveEmployeeTableLink': { method: 'POST', params: { action: 'RemoveEmployeeTableLink' } },
-                'DeleteRoomObject': { method: 'DELETE', params: { action: 'DeleteRoomObject' } },
-                'GetRooms': { method: 'GET', params: { action: 'GetRooms' }, isArray: true },
-                'ChangeRoom': { method: 'GET', params: { action: 'ChangeRoom' } },
-                'CreateNewRoom': { method: 'GET', params: { action: 'CreateNewRoom' } },
+            return $resource(':action', {}, {
+                'Get': { method: 'GET', params: { action: '/Map/Get' } },
+                'SaveRoom': { method: 'POST', params: { action: '/Map/SaveRoom' } },
+                'SaveWall': { method: 'POST', params: { action: '/Map/SaveWall' } },
+                'SaveTable': { method: 'POST', params: { action: '/Map/SaveTable' } },
+                'SaveEmployeeTableLink': { method: 'POST', params: { action: '/Map/SaveEmployeeTableLink' } },
+                'RemoveEmployeeTableLink': { method: 'POST', params: { action: '/Map/RemoveEmployeeTableLink' } },
+                'DeleteRoomObject': { method: 'DELETE', params: { action: '/Map/DeleteRoomObject' } },
+                'GetRooms': { method: 'GET', params: { action: '/Map/GetRooms' }, isArray: true },
+                'ChangeRoom': { method: 'GET', params: { action: '/Map/ChangeRoom' } },
+                'CreateNewRoom': { method: 'GET', params: { action: '/Map/CreateNewRoom' } },
             });
-        }])
- .factory('EmployeeProvider', [
-            '$resource', function($resource) {
-                return $resource('/Map/:action', {}, {
-                    'query': { method: 'GET', params: { action: 'GetEmployeesWithoutSeat' }, isArray: true }
-                });
-            }]);
+        }
+    ])
+    .factory('EmployeeProvider', [
+        '$resource', function($resource) {
+            //return $resource('localhost:44039/Map/GetEmployeesWithoutSeat');
+            return $resource(':action', {}, {
+                'query': { method: 'GET', params: { action: '/Map/GetEmployeesWithoutSeat' }, isArray: true }
+            });
+       
+ }]);
